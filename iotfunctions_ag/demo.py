58e333a90a88fa1983fdbc0d7065b4bb82606299
Alberto Girolami <alberto.girolami87@gmail.com>
2018-10-30 18:05:45 +0100

    added ToParquet function

--- iotfunctions_ag/demo.py
+++ iotfunctions_ag/demo.py
@@ -15,6 +15,10 @@ import numpy as np
 import pandas as pd
 import numbers
 import datetime as dt
+import pyarrow.parquet as pq
+import pyarrow as pa
+import s3fs
+from pyarrow.filesystem import S3FSWrapper
 from .preprocessor import BaseTransformer
 
 PACKAGE_URL = 'git+https://github.com/agirolami87/functions.git'
@@ -58,6 +62,38 @@ class TiltCalcString(BaseTransformer):
         for r,c in enumerate(a):
             df.loc[r,self.output_item] = '['+str(np.arcsin(float(c[1])/9.8))+','+str(np.arcsin(float(c[2])/9.8))+']'
         return df
+    
+class ToParquet(BaseTransformer):
+    '''
+    Denormalize and save as parquet file in COS.
+    '''
+    
+    def __init__(self, sens_pos, X, Y, Z, cos_credentials, output_status):
+        self.sens_pos = sens_pos
+        self.X = X
+        self.Y = Y
+        self.Z = Z
+        self.cos_credentials= cos_credentials
+        self.output_status = output_status
+        super().__init__()
+
+    def execute (self, df):
+        df = df.copy()
+        bucket = 'iotcs-as-bucket'
+        path = 's3test-alberto-%s.parquet' %(dt.datetime.now().isoformat())
+        bucket_uri = '{bucket}/{path}'.format(**{'bucket':bucket, 'path': path})
+        sink = fs.open(bucket_uri, 'wb')
+        df2 = []
+        for sens in df[self.sens_pos].drop_duplicates():
+            df1 = pd.concat([pd.Series([int(element) for list_ in df[df[self.sens_pos]==sens][axis].str.split(',').values for element in list_]).rename(axis) for axis in [self.X,self.Y,self.Z]], axis=1)
+            df1[self.sens_pos] = sens
+            df2.append(df1)
+        df2 = pd.concat(df2, ignore_index=True)
+        ta=pa.Table.from_pandas(df2)
+        pw = pq.ParquetWriter(sink, schema=ta.schema)
+        pw.write_table(ta)
+        df[self.output_status]=bucket_uri
+        return df  
 
 
 
