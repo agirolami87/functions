e6b5ba094d6b49eb4672ade850fc39b13ff5420d
Alberto <alberto@MacBook-Pro-di-Alberto.local>
2018-10-31 09:53:41 +0100

    update library name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='iotcsfunctions_ag',
+    name='iotcsfunctionsAG',
     version='1.0.8',
     packages=find_packages(),
     install_requires=['dill','numpy','pyarrow','s3fs'],
