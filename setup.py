38e61f9168a1d9a50f0e4a6c94ab34deed452c79
Alberto <alberto@MacBook-Pro-di-Alberto.local>
2018-10-31 09:47:28 +0100

    update setup

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='iotcsfunctions',
+    name='iotcsfunctions_ag',
     version='1.0.8',
     packages=find_packages(),
-    install_requires=['dill','numpy','pyarrow','s3fs'
-    ],
+    install_requires=['dill','numpy','pyarrow','s3fs'],
 )
